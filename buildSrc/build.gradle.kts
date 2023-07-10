72d571dd0cd75625ffa3fe46e66a4441ce913443
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-07-10 13:24:19 +0100

    Bump versions
     * Detekt -> `1.23.0`
     * Protobuf Plugin -> `0.9.3`
     * Kotlin plugins -> `1.8.22`

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -62,7 +62,7 @@ val grGitVersion = "4.1.1"
  * Please check that this value matches one defined in
  *  [io.spine.internal.dependency.Kotlin.version].
  */
-val kotlinVersion = "1.8.10"
+val kotlinVersion = "1.8.22"
 
 /**
  * The version of Guava used in `buildSrc`.
@@ -90,7 +90,7 @@ val errorPronePluginVersion = "3.0.1"
  * @see <a href="https://github.com/google/protobuf-gradle-plugin/releases">
  *     Protobuf Gradle Plugins Releases</a>
  */
-val protobufPluginVersion = "0.9.2"
+val protobufPluginVersion = "0.9.3"
 
 /**
  * The version of Dokka Gradle Plugins.
@@ -107,7 +107,7 @@ val dokkaVersion = "1.8.10"
  *
  * @see <a href="https://github.com/detekt/detekt/releases">Detekt Releases</a>
  */
-val detektVersion = "1.22.0"
+val detektVersion = "1.23.0"
 
 configurations.all {
     resolutionStrategy {
